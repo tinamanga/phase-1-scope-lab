a415bf22b4a92b59ff590d0ece91b24d3bc7d3f8
tinamanga <christinamanga28@gmail.com>
2025-03-17 15:14:29 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Global variable for customerName
+var customerName = 'bob';
+
+// Function to upper case the customer name
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Fix the setBestCustomer function to correctly set the global bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Implicitly creates a global bestCustomer variable
+}
+
+// Function to overwrite the bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; // Overwrites the global bestCustomer variable
+}
+
+// Constant in global scope for leastFavoriteCustomer
+const leastFavoriteCustomer = 'John Doe';
+
+// Function that tries to change the constant (will throw an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane Doe'; // This will throw an error since leastFavoriteCustomer is a constant
+}
